533e32f730d7c4ff3c028895f2b95727672c0a96
marykaweb <marykaweb@gmail.com>
2017-12-14 23:43:19 +0300

    1412

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"
+    <title>JS задачи</title>
 </head>
 <body>
 <a href="page1.html">Уроки по JS</a>
--- script.js
+++ script.js
@@ -44,3 +44,29 @@ function areaCircle(obj) {
     var с=obj.c1.value;
     obj.res2.value=3.14*с*с;
 }
+
+function averageValue(obj) {
+    var v1=1*obj.m1.value;
+    var v2=1*obj.m2.value;
+    var v3=1*obj.m3.value;
+    var v4=1*obj.m4.value;
+    var v5=1*obj.m5.value;
+    var v6=1*obj.m6.value;
+    var v7=1*obj.m7.value;
+    var v8=1*obj.m8.value;
+    var v9=1*obj.m9.value;
+    var v10=1*obj.m10.value;
+    var v11=1*obj.m11.value;
+    var v12=1*obj.m12.value;
+
+    obj.res8.value=(v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12)/12;
+}
+
+function bigPic(){
+    var w=document.tigrenok.width;
+    if (w<302){
+        document.tigrenok.width=w+10;
+        document.tigrenok.src="img/tigrenok.jpg";
+        setTimeout("bigPic()", 500);
+    }
+}
